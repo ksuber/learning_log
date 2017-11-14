fb3a669f946df6f4084cb0c31926ead33f1e3a0c
ksuber <k_suber52@yahoo.com>
2017-11-13 21:32:59 -0500

    Heroku deployment push 7

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'q2lteih!z6_a*hv1doo5q1vi*hq$kk#k4n5#3%nm0vu*#vtz73'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['arcane-tor-64421.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
