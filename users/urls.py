375484fc39cb7e017203e0868414a8dd754e87d2
ksuber <k_suber52@yahoo.com>
2017-11-13 19:00:41 -0500

    Heroku deployment push

---
+++ users/urls.py
@@ -0,0 +1,16 @@
+''' Url patterns for users'''
+
+from django.conf.urls import url
+from django.contrib.auth.views import login
+from . import views
+
+urlpatterns = [
+    # Login page.
+    url(r'^login/$', login, {'template_name': 'users/login.html'},
+        name='login'),
+    # Lougout Page
+    url(r'^logout/$', views.logout_view, name='logout'),
+    # Registration page
+    url(r'^register/$', views.register, name='register')
+
+]
